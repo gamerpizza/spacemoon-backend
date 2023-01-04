b5e0b89606b7f0ae76a8b96007f2f6de2e9213e1
rogelioConsejo <rogelio.consejo@gmail.com>
2023-01-04 17:29:21 +0100

    changelog

--- server/spacemoon.go
+++ server/spacemoon.go
@@ -19,7 +19,7 @@ import (
 
 func main() {
 	log.Default().Print("starting spacemoon/bubblegum server 🚀")
-	log.Default().Print("v1.2.1")
+	log.Default().Print("v1.2.2")
 	setupHandlers()
 	listenAndServe()
 }
